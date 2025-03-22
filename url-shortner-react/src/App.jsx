59174ac877eb346068076f9120ceb46d01c0e66d
Manish_Syal <manishsyal77@gmail.com>
2025-03-22 14:02:31 +0530

    setup the frontend with rect+vite and configured the tailwind css using PostCss

---
+++ url-shortner-react/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline text-amber-200">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
